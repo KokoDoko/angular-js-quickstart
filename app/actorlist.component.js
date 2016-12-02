083023d7684c4256874eb63cc7f968440c7aaa64
KokoDoko <benni_jolink@hotmail.com>
2016-12-02 16:32:25 +0100

    Simplified fetch

--- app/actorlist.component.js
+++ app/actorlist.component.js
@@ -15,39 +15,12 @@
       },
 
       searchActor: function(){
-        // this function uses window.fetch to find star wars actors
-
-        fetch('http://swapi.co/api/people/1/', {
-            method: 'get',
-            headers: {
-                'Accept': 'application/json'
-            }
+        fetch('http://swapi.co/api/people/1/')
+          .then(res => res.json())
+          .then(res => {
+            // here you can pass the json result to your own function
+            console.log(res);
           })
-          .then(this.processStatus)
-          .then(this.parseJson)
-          .then((js) => {
-            // here you can send the js object to your own function
-            // this.myFunction(js);
-            console.log("we received data");
-            console.log(js);
-          })
-          .catch((err) => {
-            console.log("Error " + err);
-          })
-      },
-
-      // we have to check if fetch returned a 200 or a 404
-      processStatus : function (response) {
-          if (response.status === 200 || response.status === 0) {
-              return Promise.resolve(response)
-          } else {
-              return Promise.reject(new Error(response.statusText))
-          }
-      },
-
-      // interpret the result as json
-      parseJson : function (response) {
-          return response.json();
       }
 
     });
